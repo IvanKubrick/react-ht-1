d7342c5b1a328556db2761cd602615205a7cb056
Ivan Hrushevich <ivan_hrushevich@epam.com>
2019-04-10 19:41:51 +0300

    Create CharComponent

--- src/App.js
+++ src/App.js
@@ -2,24 +2,48 @@ import React, { Component } from "react";
 import "./App.css";
 
 import ValidationComponent from "./ValidationComponent/ValidationComponent";
+import CharComponent from "./CharComponent/CharComponent";
 
 class App extends Component {
   state = {
+    textValue: "",
     textLength: 0
   };
 
   handleInputChange = event => {
-    const length = event.target.value.length;
+    const value = event.target.value;
 
-    this.setState({ textLength: length });
+    this.setState({
+      textValue: value,
+      textLength: value.length
+    });
+  };
+
+  removeChar = index => {
+    const text = this.state.textValue;
+    const updatedText = text.slice(0, index) + text.slice(index + 1);
+
+    this.setState({
+      textValue: updatedText,
+      textLength: this.state.textLength - 1
+    });
   };
 
   render() {
+    const chars = this.state.textValue.split("").map((char, index) => {
+      return <CharComponent char={char} click={() => this.removeChar(index)} />;
+    });
+
     return (
       <div className="App">
-        <input type="text" onChange={this.handleInputChange} />
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.textValue}
+        />
         <p>{this.state.textLength}</p>
         <ValidationComponent length={this.state.textLength} />
+        {chars}
       </div>
     );
   }
